26af118405fdec1bc9ac3d8756a8b961dd1e4a1b
luqinmao <1002438809@qq.com>
2017-11-29 15:34:03 +0800

    retrofit传参

--- app/src/main/java/com/werb/gankwithzhihu/api/NewsApi.java
+++ app/src/main/java/com/werb/gankwithzhihu/api/NewsApi.java
@@ -2,7 +2,8 @@ package com.werb.gankwithzhihu.api;
 
 import com.werb.gankwithzhihu.bean.news.NewsData;
 
-import retrofit2.http.GET;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -14,7 +15,17 @@ public interface NewsApi {
 
     //http://v.juhe.cn/toutiao/index
     //type=top&key=d050ce77241cf978f252dbd4db9ba00f
-    @GET("index")
+//    @GET("index")
+//    Observable<NewsData> getNewsData(@Query("type") String type, @Query("key") String key);
+
+    @POST("index")
     Observable<NewsData> getNewsData(@Query("type") String type, @Query("key") String key);
+
+    @POST("index")
+    Observable<NewsData> reportComment(
+            @Query("access_token") String access_token,
+            @Body NewsData bean);
+
+
 
 }
